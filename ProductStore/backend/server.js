1f2a5fd099c15d2912e3f04164948bbe73bd02ac
Shrey Choudhary <shreyc1907@gmail.com>
2025-04-18 15:49:39 +0530

    GET, PUT, POST & DELETE API created

--- ProductStore/backend/server.js
+++ ProductStore/backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.json()); // This is middleware that allows to accept JSON data in the req.body.
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
