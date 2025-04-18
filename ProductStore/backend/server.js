0ca27bb4b6a910e46428ac33c70c66daf8f8f17c
Shrey Choudhary <shreyc1907@gmail.com>
2025-04-18 17:08:58 +0530

    PORT added in the .env

--- ProductStore/backend/server.js
+++ ProductStore/backend/server.js
@@ -6,12 +6,13 @@ import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // This is middleware that allows to accept JSON data in the req.body.
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server Started at http://localhost:5000");
+  console.log("Server Started at http://localhost:" + PORT);
 });
